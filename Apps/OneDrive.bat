f644e490dd24ca258981283e51176f78ddbc304d
Satxm <56530136+Satxm@users.noreply.github.com>
2024-05-09 15:38:19 +0800

    Delete Apps directory

--- Apps/OneDrive.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title OneDrive
-
-if exist OneDriveSetup.exe del /q /f OneDriveSetup.exe
-
-..\bin\aria2c --no-conf -x16 -s16 -j5 -c -R --allow-overwrite=true --auto-file-renaming=false  -d. "https://g.live.com/1rewlive5skydrive/WinProdLatestBinary"
